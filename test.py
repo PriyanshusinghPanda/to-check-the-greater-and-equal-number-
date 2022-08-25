ec2a2d86bf4c5b849311126e35f863259f465032
PriyanshusinghPanda <79511589+PriyanshusinghPanda@users.noreply.github.com>
2022-08-25 22:39:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -17,14 +17,10 @@ if b>=a and b>=c:
         print("b is greater")
     if a>b and a>c and a!=b and a!=c and a==c:
         print("b is greater and a and c are equal")
-    if a==b==c:
-        print("all are equal")
     if b==c and b!=a:
         print("b is greater than a and equal to c")
 if c>=b and c>=a:
     if a>b and a>c and a!=b and a!=c and a!=b:
-        print("a is greater")
+        print("c is greater")
     if c>b and c>a and c!=b and c!=b and a==b:
         print("c is greater and a and b are equal")
-    if a==b==c:
-        print("all are equal")
